176357c26a5543a274fff2f5e904b8ac29383e97
Guilherme Torres <guilan.torres@gmail.com>
2025-11-14 22:38:41 -0300

    feat: add dtos

---
+++ src/main/java/com/github/guilantorres/device/dto/CreateDeviceRequestDTO.java
@@ -0,0 +1,12 @@
+package com.github.guilantorres.device.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateDeviceRequestDTO {
+
+  private String name;
+  private String brand;
+}
